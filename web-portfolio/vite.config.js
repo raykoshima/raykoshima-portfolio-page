7524483ace6fe8cf10d4eeaa26d5f2badd9700a2
NekoSakuraLucia <mokurolucia@gmail.com>
2025-02-03 11:16:09 +0700

    นำ Sitemap ออกชั่วคราวจาก vite.config.js

--- web-portfolio/vite.config.js
+++ web-portfolio/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), Sitemap()],
+  plugins: [react()],
 });
