f15ccf79a0f0a8980e0ebe964f2d52d24431cfdd
raykoshima <truemoneythailand@gmail.com>
2025-02-05 22:52:51 +0700

    allow host

--- web-portfolio/vite.config.js
+++ web-portfolio/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: process.env.VITE_ALLOWED_HOSTS
+      ? process.env.VITE_ALLOWED_HOSTS.split(',')
+      : [],
+    host: true,
+    port: 5173
+  }
 });
