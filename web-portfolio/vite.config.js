bc5def5425cd12037d2246e40112081480124caa
raykoshima <truemoneythailand@gmail.com>
2025-02-05 22:59:37 +0700

    raykoshima.xyz

--- web-portfolio/vite.config.js
+++ web-portfolio/vite.config.js
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: process.env.VITE_ALLOWED_HOSTS
-      ? process.env.VITE_ALLOWED_HOSTS.split(',')
-      : [],
+    allowedHosts: [
+      'raykoshima.xyz',
+      'x08wccg0g0csk08o4cko08kw.68.183.186.199.sslip.io' // Add your host here
+    ],
     host: true,
     port: 5173
   }
