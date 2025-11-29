d2597b46005e03d5471c31547110dfeb0fcaa1cd
ray <64307199+raykoshima@users.noreply.github.com>
2025-11-29 18:29:11 +0700

    Update allowedHosts in Vite configuration

--- web-portfolio/vite.config.js
+++ web-portfolio/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     allowedHosts: [
       'raykoshima.xyz',
-      'x08wccg0g0csk08o4cko08kw.68.183.186.199.sslip.io' // Add your host here
+      'x08wccg0g0csk08o4cko08kw.68.183.186.199.sslip.io', // Add your host here
+      'uwssok0wo0cs048cw4sok084.185.241.210.72.sslip.io'
     ],
     host: true,
     port: 5173
